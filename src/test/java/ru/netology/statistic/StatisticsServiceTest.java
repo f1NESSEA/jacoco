37d4d71755a3834f026cd1105b757ef259a1fc11
Anton Titov <anton.titov97@gmail.com>
2023-03-27 22:51:45 +0300

    tests added

--- src/test/java/ru/netology/statistic/StatisticsServiceTest.java
+++ src/test/java/ru/netology/statistic/StatisticsServiceTest.java
@@ -17,4 +17,28 @@ public class StatisticsServiceTest {
 
         Assertions.assertEquals(expected, actual);
     }
+
+    @Test
+    void findMin() {
+        StatisticsService service = new StatisticsService();
+        long[] incomesInBillions = {-12, -5, -8, -4, -5, -3, -8, -6, -11, -12, 0};
+        long expected = 0;
+
+        long actual = service.findMax(incomesInBillions);
+
+        Assertions.assertEquals(expected, actual);
+
+    }
+
+    @Test
+    void findMaxMid() {
+        StatisticsService service = new StatisticsService();
+        long[] incomesInBillions = {9, 5, 8, 12, 5, 3, 8, 6, 11, 12, 0};
+        long expected = 12;
+
+        long actual = service.findMax(incomesInBillions);
+
+        Assertions.assertEquals(expected, actual);
+
+    }
 }
